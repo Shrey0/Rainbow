a7be9a21a68919dc28ec7987d728d1bf2540d8d8
Shrey Jagad <40837247+Shrey0@users.noreply.github.com>
2020-03-20 17:36:00 +0530

    Add files via upload

---
+++ Rainbow Infinity/js/index.js
@@ -0,0 +1,11 @@
+//code borrowed from another codepen: https://codepen.io/kathykato/pen/rwNqjP*//
+
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
